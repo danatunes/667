3f5a47f3cfb7d24164d80f6b1099a21377dbc9b0
Magzhan <codebuster@bk.ru>
2022-03-30 04:24:50 +0600

    asdasd

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
